3d1be12520e61143f16528607d944b9ed9b9ab06
Ricard Anaya <ricardoanayamoreno@hotmail.com>
2021-06-10 22:10:23 -0500

    Nuevos cambios Ejercicios 7 y 8

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -1,46 +1,81 @@
 // Do not change any of the function names
 
 function mayuscula(nombre) {
-  //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
-  //ej: Recibe "mario" ----> Devuelve "Mario"
+  //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra 
+  //en mayúscula
+  //ej: Recibe "mario " ----> Devuelve // es un String = "Mario"
   //Tu código:
+  return nombre[0].toUpperCase() + nombre.slice(1);
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb();  
 }
 
 function operacionMatematica(n1, n2, cb) {
-  //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
-  //Devolver el callback pasándole como argumentos los números recibidos.
+  //Vamos a recibir una función que realiza una operación 
+  //matemática como callback (cb = es una funcion) junto con dos números (n1, n2).
+  //Devolver (return) el callback (cb) pasándole como argumentos los números 
+  //recibidos (n1, n2).
   //Tu código:
+  return cb(n1, n2);
 }
 
 function sumarArray(numeros, cb) {
-  // Suma todos los números enteros (int/integers) de un array ("numeros")
+  // Suma todos los números enteros (int/integers) de un array ("numeros[1,2,3,4,5,6]")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+
+  //sintaxis:
+  //arr.reduce(callback(acumulador, valorActual[, índice[, array]])[, valorInicial])
+  
+  let suma = numeros.reduce(function(acumulador, i){
+    return i + acumulador
+  })
+  cb(suma)
 }
 
 function forEach(array, cb) {
-  // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
+  // Itera sobre la matriz "array" y pasa los valores al callback 'uno por uno'
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
-  //Tu código:
-}
+  //Tu código:  
+    for(var i = 0; i < array.length; i++) {
+    cb(array[i]);
+  }  
+} 
 
 function map(array, cb) {
   // Crea un nuevo array
-  // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
+  // Itera sobre cada valor en "array", pásalo a `cb` y 
+  //luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+    var nuevoArray = array.map(function(array){
+    return cb(array)
+  })
+  return nuevoArray
+
+  //var nuevoArray = [];
+  //for(let i = 0; i < array.length; i++){
+  //if(array[i]) {
+  //nuevoArray
+  //}
+  //}
+
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  const nuevoArray = array.filter(function(array) {
+    return array.charAt() === "a";
+  })
+  
+  return nuevoArray
 }
 
 // No modificar nada debajo de esta línea
--- 08-JS-Extra-Homeworks/homework/homework.js
+++ 08-JS-Extra-Homeworks/homework/homework.js
@@ -1,40 +1,78 @@
 // No cambies los nombres de las funciones.
 
 function deObjetoAmatriz(objeto){
-  // Escribe una función que convierta un objeto en una matriz, donde cada elemento representa 
-  // un par clave-valor en forma de matriz.
+  // Debe devolver una matriz, donde cada elemento representa un par 
+  //clave-valor en forma de matriz.
+
+  // Escribe una función que convierta un objeto en una matriz, donde 
+  //cada elemento representa 
+  // un par clave-valor en forma de matriz (array).
   //Ejemplo: 
   /*objeto({
       D: 1,
       B: 2,
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
-  //Escribe tu código aquí
+   //Escribe tu código aquí
+  return Object.entries(objeto)
 }
 
 
 function numberOfCharacters(string) {
-  //La función recibe un string. Recorre el srting y devuelve el caracter con el número de veces que aparece 
+  //Debe devolver un objeto con los caracteres y el número
+  //de veces aparecen como un par clave-valor
+  
+  //La función recibe un string. Recorre el srting y devuelve el caracter 
+  //con el número de veces que aparece 
   //en formato par clave-valor.
-  //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
+  //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, 
+  //c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  
+  var objeto = {}; 
+    for(var i in string){
+        objeto[string[i]] = ( objeto[string[i]] || 0 ) + 1; // Incrementamos el valor 
+    }                                                       //si el elemento ya existe 
+  return objeto;
 }
 
 
+
 function capToFront(s) {
-  //Realiza una función que reciba como parámetro un string y mueva todas las letras mayúsculas
-  //al principio de la palabra.
+  //Debe devolver todas las letras mayúsculas al principio de la palabra.
+
+  //Realiza una función que reciba como parámetro un string y mueva todas las 
+  //letras mayúsculas al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  let caps = ''
+  let min = ''
+  for (let i = 0; i < s.length; i++){
+    if (s[i].toUpperCase() === s[i]){
+      caps = caps + s[i]
+    } else {
+      min = min +s[i]
+    }
+  }
+  return caps + min
 }
 
 
 function asAmirror(str) {
   //La función recibe una frase. 
-  //Escribe una función que tome la frase recibida y la devuelva de modo tal que se pueda leer de izquierda a derecha 
+  //Escribe una función que tome la frase recibida y la devuelva de modo 
+  //tal que se pueda leer de izquierda a derecha 
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
-  //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
+  //Ej: Recibe ---> "The Henry Challenge is close!" || 
+  //Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  let palabras = str.split(' ')
+
+  let reversed = palabras.map(function(elemento){
+    return elemento.split('').reverse().join('')
+  })
+  let result = reversed.join(' ')
+  return result
 } 
 
 
@@ -43,29 +81,81 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  let strNumero = new String(numero)
+  for (let i = 0; i < strNumero.length; i++){
+    for (let j = strNumero.length -1; j >= 0; j--){
+      if (strNumero[i] !== strNumero[j]){
+      return "No es capicua"
+    }
+    i++
+  }
+ }
+ return "Es capicua"
 }
+
 
 
 function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+
+//  let cadenaSinABC(cadena){
+//    for (let i = 0; i < cadena.length; i++){
+//      if(cadena[i].toLowerCase() !=='a' && cadena[i].toLowerCase() !=='b' && cadena[i].toLowerCase() !=='c'){
+//        cadenaSinABC = cadenaSinABC + cadena[i];
+//    }
+//  }
+//}
+
+//    return cadenaSinABC
+    
+  
 }
 
 
+
 function sortArray(arr) {
-  //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
-  //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
+  //La función recibe una matriz de strings. Ordena la matriz en orden 
+  //creciente de longitudes de cadena
+  //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || 
+  //Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+
+  let sortedArray = [];
+  let aux = ''
+  
+  for (let i = 0; i < arr.length; i++){
+    for (let j = i + 1; j < arr.length; j++){
+      if (arr[i].length > arr[j].length){
+        aux = arr[i]     // Se guardó en el auxiliar
+        arr[i] = arr[j]  // Copio lo que tengo en j a i
+        arr[j] = aux     // Recupero el 'i' del auxiliar y lo pongo en 'j'
+      }
+    }
+  }
+return arr
 }
 
 
 function buscoInterseccion(arreglo1, arreglo2){
-  //Existen dos arrays, cada uno con 5 números. A partir de ello, escribir una función que permita 
-  //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
+  //Existen dos arrays, cada uno con 5 números. A partir de ello, escribir una
+  // función que permita 
+  //retornar un nuevo array con la intersección de ambos elementos. 
+  //(Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+
+  let result = []
+  for (let i=0; i<arreglo1.length;i++){
+    for (let j=0; j<arreglo2.length;i++){
+      if (arreglo1[i] === arreglo2[j]){
+        result.push(arreglo1[i])
+      }
+    }
+  }
+  return result
 }
 
 
